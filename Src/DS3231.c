fc02221393735631a68c151eef934fb9d22c166c
chillmf <chillmf20@gmail.com>
2024-12-05 05:47:19 -0600

    Update DS3231.c
    Added display_Real_Date(0, 240); to display_Real_Time for auto
    date change.

--- Src/DS3231.c
+++ Src/DS3231.c
@@ -163,6 +163,7 @@ void display_RealTime(int x, int y) {
 	getTime(&rtc_hour, &rtc_minute, &rtc_second, &rtc_ampm, _24_hour_format);
 	if (rtc_hour < old_rtc_hour) {
 		getDate(&rtc_dow, &rtc_date, &rtc_month, &rtc_year);
+		display_Real_Date(0, 240);
 		make_File_Name();
 	}
 	show_UTC_time(x, y, rtc_hour, rtc_minute, rtc_second, 0);
