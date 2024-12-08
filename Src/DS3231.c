d2e8ecd79848cc664a2dec1fd989ff87b1508672
chillmf <chillmf20@gmail.com>
2024-12-08 08:49:41 -0600

    Update DS3231.c
    Replaced  make_File_Name(); with Init_Log_File();

--- Src/DS3231.c
+++ Src/DS3231.c
@@ -164,7 +164,8 @@ void display_RealTime(int x, int y) {
 	if (rtc_hour < old_rtc_hour) {
 		getDate(&rtc_dow, &rtc_date, &rtc_month, &rtc_year);
 		display_Real_Date(0, 240);
-		make_File_Name();
+		//make_File_Name();
+		Init_Log_File();
 	}
 	show_UTC_time(x, y, rtc_hour, rtc_minute, rtc_second, 0);
 }
